2d6b9c939ba59224c4a6a4bdf12ae73c582a0053
Alan Raff <alanr917@hotmail.com>
2023-09-06 01:53:39 -0400

    adjust timestamp to be more accurate when saving recording

--- tracker.py
+++ tracker.py
@@ -91,7 +91,7 @@ def main():
 			execute_command("ffmpeg -f s16le -ac 1 -ar " + str(config["sample_rate"]) + "000 -acodec pcm_s16le -i recordings/" + timestamp_epoch + ".raw recordings/" + timestamp_epoch + ".mp3")
 			execute_command("rm -rf recordings/" + timestamp_epoch + ".iq recordings/" + timestamp_epoch + ".raw")
 			update_audio_file(timestamp_readable, timestamp_epoch)
-			append_to_log("logs/tracker_output.log", "[" + timestamp_readable + "] Saved recording to: recordings/" + timestamp_epoch + ".mp3" + "\n")
+			append_to_log("logs/tracker_output.log", "[" + datetime.now().strftime("%m-%d-%Y %H:%M:%S") + "] Saved recording to: recordings/" + timestamp_epoch + ".mp3" + "\n")
 
 		print("Sleeping for " + str(config["interval_seconds"]) + " seconds.")
 		time.sleep(config["interval_seconds"])
