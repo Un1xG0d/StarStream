f5503bb8eb7959fc68cf9dabfd288ab689f20248
Alan Raff <alanr917@hotmail.com>
2023-09-07 12:57:25 -0400

    reorder and rename routing functions

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ app = Flask(__name__)
 user_location = get_user_location()
 
 @app.route("/")
-def index():
+def dashboard():
 	iss_location = get_iss_location()
 	distance = round(get_distance_between(user_location, iss_location), 1)
 	iss_geocoded_location = get_geocoded_location(iss_location)
@@ -55,12 +55,12 @@ def index():
 			unique_dates.append(recording["timestamp"].split(" ")[0])
 	return render_template("index.html", distance=distance, iss_location=iss_geocoded_location, recordings=recordings, unique_dates=unique_dates)
 
-@app.route("/logs")
-def logs():
-	tracker_output = read_file("logs/tracker_output.log").split("\n")
-	return render_template("logs.html", logs=tracker_output)
-
 @app.route("/recordings")
 def recordings():
 	recordings = load_json("logs/recordings.json")
 	return render_template("recordings.html", recordings=recordings)
+
+@app.route("/logs")
+def logs():
+	tracker_output = read_file("logs/tracker_output.log").split("\n")
+	return render_template("logs.html", logs=tracker_output)
