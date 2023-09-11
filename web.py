757c4cf0e866c6ef04ea40a3a36a748192e61305
Alan Raff <alanr917@hotmail.com>
2023-09-11 19:41:49 -0400

    relocate function call

--- web.py
+++ web.py
@@ -49,10 +49,10 @@ app = Flask(__name__)
 agps_thread = AGPS3mechanism()
 agps_thread.stream_data()
 agps_thread.run_thread()
-user_location = get_user_location()
 
 @app.route("/")
 def dashboard():
+	user_location = get_user_location()
 	iss_location = get_iss_location()
 	distance = round(get_distance_between(user_location, iss_location), 1)
 	iss_geocoded_location = get_geocoded_location(iss_location)
